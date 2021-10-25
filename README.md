a4bc3953c49b9acbcd5747dbe4a62ee73241d38a
ManikandanKumarkp <manikandankumarkp@gmail.com>
2021-10-25 08:50:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-ev7tjf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-ev7tjf)
---
+++ index.js
@@ -0,0 +1,13 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+
+//map
+//helper method :set,get,has,delete ,size
+let mymap = new map();
+mymap.set('mani'[1,2,3,4,]);
+console.log(mymap.get(1))
