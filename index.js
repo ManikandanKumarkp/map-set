d6561ea58dfddd0015c14af7d593710ffbf9d3ce
ManikandanKumarkp <manikandankumarkp@gmail.com>
2021-10-25 09:10:46 +0530

    1 file modified

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 //map
 //helper method :set,get,has,delete ,size
-let mymap = new map();
-mymap.set('mani'[1,2,3,4,]);
-console.log(mymap.get(1))
+// let mymap = new Map();
+// mymap.set('mani',{1:2,3:4})
+// console.log(mymap.get('mani'))
+
+
+let nme=new Map();
+nme.set('x',[{mani:'sasi',age:23,car:'fix'},{mani:'sasi',age:23,car:'fix'},{mani:'sasi',age:23,car:'fix'},{mani:'sasi',age:23,car:'fix'}])
+console.log(nme.has('x'))
+console.log(nme.delete('x'))
+
+console.log(nme.has('x'))
+console.log(nme.get('x'))
+
+
+let myob=new Map();
+myob.set('x',[{mani:'sasi',age:23,car:'fix'},{mani:'sasi',age:23,car:'fix'},{mani:'sasi',age:23,car:'fix'},{mani:'sasi',age:23,car:'fix'}])
+console.log(myob.get('x'))
+
+console.log(myob.size)
+
+
+//set  helper method add,delete,has,size
+
+
+
